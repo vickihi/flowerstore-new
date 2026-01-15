42d43aee8f968983d075b62cf2b9a6d972359270
Weiwei Guo <vickihi2000@gmail.com>
2026-01-15 13:03:17 -0500

    Add product model and related configurations

--- flowerproducts/models.py
+++ flowerproducts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    ...
+
+class Product(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='flowerproducts/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at', 'name']
+
+    def __str__(self):
+        return self.name
