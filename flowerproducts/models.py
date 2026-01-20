4045705e59889ef3189ed0767f2bddc497e4182a
Weiwei Guo <vickihi2000@gmail.com>
2026-01-20 11:29:24 -0500

    Update product model and add dummy_data

--- flowerproducts/models.py
+++ flowerproducts/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -17,12 +15,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='flowerproducts/')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField("created at", auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     class Meta:
