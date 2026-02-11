533a70b532f7311b5fc48aa1142072f9f02dbe70
XiaH-dev <6367321@edu.vaniercollege.qc.ca>
2026-02-11 10:55:06 -0500

    adjust format

--- flowerproducts/migrations/0003_remove_product_views_count.py
+++ flowerproducts/migrations/0003_remove_product_views_count.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("flowerproducts", "0002_product_views_count"),
     ]
--- reviews/migrations/0006_flag_created_at.py
+++ reviews/migrations/0006_flag_created_at.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("reviews", "0005_remove_flag_created_at_alter_flag_flag"),
     ]
