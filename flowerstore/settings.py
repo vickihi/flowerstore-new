a835080f6886790d9d128af2feaa88e59dc82553
Weiwei Guo <vickihi2000@gmail.com>
2026-04-15 09:53:06 -0400

    fix static storage

--- flowerstore/settings.py
+++ flowerstore/settings.py
@@ -161,7 +161,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
